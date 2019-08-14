e146d97dc2a39090387490b06dadc3f5812b6b31
aemelius <simoneperfect@gmail.com>
2019-08-14 23:23:25 +0200

    .

---
+++ working.groovy
@@ -0,0 +1,53 @@
+podTemplate(yaml: """
+apiVersion: v1
+kind: Pod
+spec:
+  containers:
+  - name: docker
+    image: docker:1.11
+    command: ['cat']
+    tty: true
+    volumeMounts:
+    - name: dockersock
+      mountPath: /var/run/docker.sock
+  - name: kubedeployer
+    image: widerin/eks-helmsman:0.2
+    command: ['cat']
+    tty: true
+  volumes:
+  - name: dockersock
+    hostPath:
+      path: /var/run/docker.sock
+"""
+) {
+
+    def image = "aemel/simple-flask-1:${currentBuild.number}"
+    node(POD_LABEL) {
+        stage('Build Docker image') {
+            git 'https://github.com/aemelius/simple-flask-1.git'
+            container('docker') {
+                sh "docker build -t ${image} ."
+	       	      withCredentials([[$class: 'UsernamePasswordMultiBinding', credentialsId: 'dockerhub', usernameVariable: 'USER', passwordVariable: 'PASSWORD']]) {
+
+                    sh "docker login -u ${USER} -p ${PASSWORD}"
+                    sh "docker push ${image}"
+                }
+            }
+        }
+
+	      stage('Kubectl stuff') {
+            git 'https://github.com/aemelius/simple-flask-1.git'
+	          container('kubedeployer') {
+	       	      withCredentials([[$class: 'UsernamePasswordMultiBinding', credentialsId: 'kubedeployer1', usernameVariable: 'AWS_ACCESS_KEY_ID', passwordVariable: 'AWS_SECRET_ACCESS_KEY']]) {
+	       	          withCredentials([[$class: 'UsernamePasswordMultiBinding', credentialsId: 'kubedeployer1', usernameVariable: 'AWS_ACCESS_KEY_ID', passwordVariable: 'AWS_SECRET_ACCESS_KEY']]) {
+			              sh "aws configure set aws_access_key_id ${AWS_ACCESS_KEY_ID}"
+			              sh "aws configure set aws_secret_access_key ${AWS_SECRET_ACCESS_KEY}"
+			              sh "aws eks update-kubeconfig --name=sb1 --region=eu-west-2"
+			              sh "kubectl get namespaces"
+                    }
+                    }
+			      }
+	      }
+	  }
+
+}
